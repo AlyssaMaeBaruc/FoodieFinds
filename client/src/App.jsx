52460846bb4b2e5afaa52e77f1bd181fe8fd7d84
AlyssaMaeBaruc <159028804+AlyssaMaeBaruc@users.noreply.github.com>
2024-05-14 15:31:33 +0200

    progress: add and delete button works

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,49 +1,79 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
-  // this is for the 
-  const [ingredients, setIngredients] = useState([""]);
-  const [meal, setMealData] = useState('');
+  const apiUrl = "https://api.spoonacular.com/recipes/findByIngredients";
+  const apiKey ="c5a76b2747564a09b63ee79f924ba472";
+
+  // this is for the users to input their ingredients 
+  const [ingredientsInput, setIngredientsInput] = useState("");
+
+  //this is for adding the ingredients the user places
+  const [addedIngredients, setAddedIngredients] = useState([]);
+
+  //this is for the recipes when we press submit and find it 
+  const [recipes, setRecipes] = useState([]);
+
 
   function handleChange(e) {
-    setIngredients(e.target.value);
+    setIngredientsInput(e.target.value);
   };
 
-  useEffect(() => {
-    fetch('https://api.spoonacular.com/recipes/findByIngredients?apiKey=c5a76b2747564a09b63ee79f924ba472&ingredients=tomato,+potato')
-  //then check if my fetch worked 
+// create a function when we press find recipes button 
+  function handleSubmit() {
+    getRecipes(setRecipes);
+    console.log("Ingredients are submitted")
+  };
+
+  // create a function for appending the ingredients when we click the add button 
+  function addNewIngredients(e){
+    e.preventDefault();
+    setAddedIngredients(i => [...i, ingredientsInput]);
+    setIngredientsInput("");
+  }
+
+  // create a function for delete button whenever the user deletes an ingredient 
+  // should come from the array of ingredients the user adds = addedIngredients 
+  function deleteIngredients(index) {
+    const updatedIngredients = addedIngredients.filter((element, i) => i !== index)
+    setAddedIngredients(updatedIngredients);
+  }
+
+
+   //fetch('https://api.spoonacular.com/recipes/findByIngredients?apiKey=c5a76b2747564a09b63ee79f924ba472&ingredients=tomato,+potato')
+//   //then check if my fetch worked 
+
+const getRecipes = () => {
+ fetch(`${apiUrl}?apiKey=${apiKey}&ingredients=${recipes}`)
   .then(response => response.json())
-  .then(response => setIngredients(response))
+  .then(response => setRecipes(response))
   .catch(error => 
     console.error(error));
-//dependencies, run this code when i start the app
-  }, []);
-
-
-  // this is the API
-// const api_key = "c5a76b2747564a09b63ee79f924ba472"
-  // const options = {
-  //   method: 'GET',
-  //   headers: {
-  //      accept: 'application/json',
-  //      'X-RapidAPI-Key': 'c5a76b2747564a09b63ee79f924ba472',
-	// 	   'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
-  //   }
-  // };
-  
-  // fetch me the ingredients data 
-
+  }
+// //dependencies, run this code when i start the app
 
 
   return (
     <>
     <h1> Plan your Meal! </h1>
     <form>
-    <input type="text" placeholder = "Enter your ingredients" value = {ingredients} onChange = {handleChange} /> 
-    <button onClick = {handleSubmit}> Add </button>
+    <input type="text" placeholder = "Enter your ingredients" value = {ingredientsInput} onChange = {handleChange} /> 
+    <button onClick={(addNewIngredients)}> Add </button>
     </form>
+    <div>
+      <ul>
+        {addedIngredients.map((ingredient, index ) => (
+          <ul
+            key = {index}> {ingredient}
+            <button 
+            className = "delete-button" 
+            onClick = {() => deleteIngredients(index)}> Delete </button> 
+          </ul>
+        ))}
+      </ul>
+      <button onClick = {handleSubmit}> Find Recipes</button>
+    </div>
     
     </>
     
