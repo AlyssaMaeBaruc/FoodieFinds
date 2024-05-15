d541d6091364e1d0b021c5148ea2f9924d21d797
AlyssaMaeBaruc <159028804+AlyssaMaeBaruc@users.noreply.github.com>
2024-05-15 15:48:57 +0200

    progress: adding error message

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,9 @@ function App() {
   //this is for the recipes when we press submit and find it 
   const [recipes, setRecipes] = useState([]);
 
+  //for error 
+  const [error,setError] = useState(null);
+
 
   function handleChange(e) {
     setIngredientsInput(e.target.value);
@@ -40,22 +43,47 @@ function App() {
     setAddedIngredients(updatedIngredients);
   }
 
+
    //fetch('https://api.spoonacular.com/recipes/findByIngredients?apiKey=c5a76b2747564a09b63ee79f924ba472&ingredients=tomato,+potato')
 //   //then check if my fetch worked 
 
 const getRecipes = () => {
  fetch(`${apiUrl}?apiKey=${apiKey}&ingredients=${addedIngredients.join(",")}`)
-  .then(response => response.json())
-  .then(response => setRecipes(response))
-  .catch(error => 
-    console.error(error));
-  }
-// //dependencies, run this code when i start the app
+  .then((response) => {
+    // this is if there is something wrong with the database 
+    if(!response.ok) {
+      throw new Error ("please try again later");
+    }
+    return response.json();
+  })
+  // this is if the user places something that the database can not read
+  .then((response) => {
+    if (!response.isArray) {
+      throw new Error ("Invalid ingredient")
+    }
+    //there is no error at this point and can be set to  null 
+  setRecipes(response);
+  setError(null);
+})
+  .catch((error) => {
+    setError(error.message);
+    console.error(error);
+  });
+
+};
+
+  // const getRecipes = () => {
+  //   fetch(`${apiUrl}?apiKey=${apiKey}&ingredients=${addedIngredients.join(",")}`)
+  // // .then(response => response.json())
+  // // .then(response => setRecipes(response))
+  // // .catch(error => 
+  // //   console.error(error));
+  // // }
 
 
   return (
     <>
-    <h2> Plan your Meal </h2>
+    <h2> What is in your Fridge? </h2>
     <form>
     <input type="text" className= "search-bar" placeholder = "Enter your ingredients" value = {ingredientsInput} onChange = {handleChange} /> 
     <button onClick={(addNewIngredients)} className= "button"> Add </button>
@@ -67,15 +95,16 @@ const getRecipes = () => {
             key = {index}> {ingredient}
             <button 
             className = "delete-button" 
-            onClick = {() => deleteIngredients(index)}> Delete </button> 
+            onClick = {() => deleteIngredients(index)}> x </button> 
           </ul>
         ))}
       </ul>
       <button onClick = {handleSubmit} className= "button" > Find Recipes</button>
+      <div className= "error-message"> {error &&  
+        <h2> ERROR 404 : {error} </h2> } </div>     
       <div className= "recipe-container"> 
       {recipes.map ((recipe, index) => (
-        <ul
-          key = {index}> 
+        <ul key = {index}> 
           <h4> {recipe.title} </h4>
           <img src = {recipe.image} className= 'image-list' />
         </ul>
